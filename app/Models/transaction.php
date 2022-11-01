2f7a621a791b1f15d7d6546fa2c37768171d2fa2
ahmadfauzan <fauzana566@gmail.com>
2022-11-01 20:26:49 +0700

    update model transaction

--- app/Models/transaction.php
+++ app/Models/transaction.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $primaryKey = 'id_peminjaman';
+    protected $fillable =   [
+        'nama_mobil',
+        'tanggal_peminjaman',
+        'tanggal_pengembalian',
+        'status_peminjaman',
+    ];
 }
